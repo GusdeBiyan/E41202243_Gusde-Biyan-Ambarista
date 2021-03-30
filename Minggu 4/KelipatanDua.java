5858d9d6bac10e0f1888621cde652b7fae7071b3
Gusde Biyan Ambarista <80672280+GusdeBiyan@users.noreply.github.com>
2021-03-30 09:38:16 +0700

    Update KelipatanDua.java

--- Minggu 4/KelipatanDua.java
+++ Minggu 4/KelipatanDua.java
@@ -7,14 +7,14 @@ public class KelipatanDua {
    
     public static void main(String[] args) {
         
-        System.out.println("          DO__WHILE         ");
-        System.out.println("Bilangan kelipatan 2 (1-100)");
-        System.out.println("============================");
+        System.out.println("          DO__WHILE         ") ;
+        System.out.println("Bilangan kelipatan 2 (1-100)") ; 
+        System.out.println("============================") ;
         int i = 1;
         do{
-            System.out.print(i + "   ");
-            i += i;
-        }while(i<=100);
+            System.out.print(i + "   ") ;
+            i += i ;
+        } while ( i <= 100) ;
        
    
     }
